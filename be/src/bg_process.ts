056f3f04047ffaaee90484381df87a77bfb9a263
privSe <sopcoerik@gmail.com>
2025-10-21 19:15:20 +0300

    added return to event mapping & changed del to ltrim
    - redis includes start and stop numbers into the returned array, so changed (0, 1000) to (0, 999)
    - I return the increment request to avoid losing the events
    - changed "del" to "ltrim" to avoid completely deleting the events hash in case another event arrives while I process the first batch.

--- be/src/bg_process.ts
+++ be/src/bg_process.ts
@@ -7,18 +7,18 @@ export const beginProcess = () => {
 
   interval = setInterval(async () => {
     try {
-      const events: string[] = await client.lRange("events", 0, 1000);
+      const events: string[] = await client.lRange("events", 0, 999);
   
       await Promise.all(
         events.map(event => {
           console.log("Processing event:", event);
           const { campaign_id } = JSON.parse(event);
           
-          client.hIncrBy("campaigns", campaign_id, 1);
+          return client.hIncrBy("campaigns", campaign_id, 1);
         })
       );
       
-      await client.del("events");
+      await client.lTrim("events", 1000, -1);
 
     } catch (error) {
       console.error("Error processing events:", error);
