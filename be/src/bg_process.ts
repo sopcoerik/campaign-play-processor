f5b75da185403c82b86f9f720d5e6bb2c941619a
privSe <sopcoerik@gmail.com>
2025-11-04 08:59:15 +0200

    deleted post-merge redundant code

--- be/src/bg_process.ts
+++ be/src/bg_process.ts
@@ -4,27 +4,23 @@ const PROCESS_INTERVAL_MS = 5000;
 let interval: NodeJS.Timeout | null = null;
 
 const processEvents = async () => {
-  if (interval !== null) {
-    return;
-  }
+    try {
+      const events: string[] = await client.lRange("events", 0, 999);
+  
+      await Promise.all(
+        events.map(event => {
+          console.log("Processing event:", event);
+          const { campaign_id } = JSON.parse(event);
+          
+          return client.hIncrBy("campaigns", campaign_id, 1);
+        })
+      );
+      
+      await client.lTrim("events", events.length, -1);
 
-  try {
-    const events: string[] = await client.lRange("events", 0, 999);
-
-    await Promise.all(
-      events.map(event => {
-        console.log("Processing event:", event);
-        const { campaign_id } = JSON.parse(event);
-        
-        return client.hIncrBy("campaigns", campaign_id, 1);
-      })
-    );
-    
-    await client.lTrim("events", events.length, -1);
-
-  } catch (error) {
-    console.error("Error processing events:", error);
-  }
+    } catch (error) {
+      console.error("Error processing events:", error);
+    }
 };
 
 export const beginProcess = (): { message: string } | undefined => {
