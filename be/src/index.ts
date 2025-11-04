b799bf188733c4bae3ed9ec91b8ff3cca1e852eb
privSe <sopcoerik@gmail.com>
2025-11-04 08:39:58 +0200

    refactor before deploy

--- be/src/index.ts
+++ be/src/index.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [process.env.ORIGIN || "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
   })
 );
